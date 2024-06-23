259a41e4377b488e27311771622a85f5dc187774
n1cholaspoole <admin@n1cholaspoole.dev>
2024-06-23 07:09:08 +0300

    Returned the option_hide.js file that was mistakenly deleted.

---
+++ uptimealert/static/js/option_hide.js
@@ -0,0 +1,30 @@
+let port = document.getElementById('port-container');
+let schema = document.getElementById('schema-container');
+let hostname = document.getElementById('hostname')
+
+function hide(value) {
+  if (value === 'port') {
+    port.classList.remove('is-hidden');
+  } else {
+    port.classList.add('is-hidden');
+  }
+
+  if (value === 'http') {
+    hostname.placeholder='URL-адрес';
+    hostname.type='link';
+    schema.classList.remove('is-hidden');
+  } else {
+    hostname.placeholder='Имя хоста';
+    hostname.type='text';
+    schema.classList.add('is-hidden');
+  }
+}
+
+window.addEventListener('load', function() {
+  let type_input_value = document.getElementById('type').value;
+  hide(type_input_value)
+})
+
+document.getElementById('type').addEventListener('change', function() {
+  hide(this.value)
+})
